24479956752fd56856fa2275e6e339d656c323bb
kite707 <12201680@inha.edu>
2024-05-28 02:04:42 +0900

    feat: 소켓 서버 구현

--- index.js
+++ index.js
@@ -1,120 +1,115 @@
+const http = require("http");
+const Socket = require("websocket").server;
+const server = http.createServer(() => {});
 
-const http = require("http")
-const Socket = require("websocket").server
-const server = http.createServer(()=>{})
+server.listen(3000, () => {
+  console.log("server started on port 3000");
+});
 
-server.listen(3000,()=>{
-    //console.log("server started on port 3000") 
-})
+const webSocket = new Socket({ httpServer: server });
+let userMap = new Map();
 
-//instantiate websocket
-const webSocket = new Socket({httpServer:server})
+webSocket.on("request", (req) => {
+  const connection = req.accept();
 
-//an array to hold the users
-const users = []
+  connection.on("message", (message) => {
+    const data = JSON.parse(message.utf8Data);
+    console.log(data);
+    const user = findUser(data.name);
 
-//add some listener
-webSocket.on('request',(req)=>{
-    const connection = req.accept()
-   
-//add listener on our connection
-    connection.on('message',(message)=>{
-        const data = JSON.parse(message.utf8Data)
-        console.log(data);
-        const user = findUser(data.name) // find recieved name through socket in users arr
-       
-        switch(data.type){
-            case "store_user":
-                if(user !=null){
-                    //if user exists
-                    connection.send(JSON.stringify({
-                        type:'user already exists'
-                    }))
-                    return
+    switch (data.type) {
+      case "store_user":
+        if (user != null) {
+          connection.send(
+            JSON.stringify({
+              type: "user already exists",
+            })
+          );
+          return;
+        }
+        const newUser = {
+          name: data.name,
+          conn: connection,
+        };
+        userMap.set(data.name, newUser);
+        break;
 
-                }
-                //if user no exists
-                const newUser = {
-                    name:data.name, conn: connection
-                }
-                users.push(newUser)
-            break
+      case "start_call":
+        let userToCall = findUser(data.target);
 
-            case "start_call":
-                let userToCall = findUser(data.target) //check whether data.target is in users arr
-
-                if(userToCall){ //if userToCall is valid we want to send a feedback to the user
-                    connection.send(JSON.stringify({ 
-                        type:"call_response", data:"user is ready for call"
-                    }))
-                } else{ // no valid responser 
-                    connection.send(JSON.stringify({
-                        type:"call_response", data:"user is not online"
-                    }))
-                }
-
-            break
-            
-            case "create_offer":
-                let userToReceiveOffer = findUser(data.target)
-
-                if (userToReceiveOffer){ // if user already exists
-                    userToReceiveOffer.conn.send(JSON.stringify({
-                        type:"offer_received",
-                        name:data.name,
-                        data:data.data.sdp 
-                    }))
-                }
-            break
-                
-            case "create_answer":
-                let userToReceiveAnswer = findUser(data.target)
-                if(userToReceiveAnswer){
-                    userToReceiveAnswer.conn.send(JSON.stringify({
-                        type:"answer_received",
-                        name: data.name,
-                        data:data.data.sdp //from 58 line and this 69 line , they exchange thir session description
-                    }))
-                }
-            break
-
-            case "ice_candidate":  // after they can exchange their ice candidate
-                let userToReceiveIceCandidate = findUser(data.target)
-                if(userToReceiveIceCandidate){
-                    userToReceiveIceCandidate.conn.send(JSON.stringify({
-                        type:"ice_candidate",
-                        name:data.name,
-                        data:{
-                            sdpMLineIndex:data.data.sdpMLineIndex,
-                            sdpMid:data.data.sdpMid,
-                            sdpCandidate:data.data.sdpCandidate
-                        }
-                    }))
-                }
-            break
-
-
+        if (userToCall) {
+          connection.send(
+            JSON.stringify({
+              type: "call_response",
+              data: "user is ready for call",
+            })
+          );
+        } else {
+          connection.send(
+            JSON.stringify({
+              type: "call_response",
+              data: "user is not online",
+            })
+          );
         }
 
-    })
-    
-    connection.on('close', () =>{
-        users.forEach( user => {
-            if(user.conn === connection){  // this user is disconnected
-                users.splice(users.indexOf(user),1) // delete from users arr
-            }
-        })
-    })
+        break;
 
+      case "create_offer":
+        let userToReceiveOffer = findUser(data.target);
 
+        if (userToReceiveOffer) {
+          userToReceiveOffer.conn.send(
+            JSON.stringify({
+              type: "offer_received",
+              name: data.name,
+              data: data.data.sdp,
+            })
+          );
+        }
+        break;
 
+      case "create_answer":
+        let userToReceiveAnswer = findUser(data.target);
+        if (userToReceiveAnswer) {
+          userToReceiveAnswer.conn.send(
+            JSON.stringify({
+              type: "answer_received",
+              name: data.name,
+              data: data.data.sdp,
+            })
+          );
+        }
+        break;
 
-
-})
-
-const findUser = username =>{ //iterate the users array and find the specific user and return
-    for(let i=0; i<users.length;i++){
-        if(users[i].name === username)
-        return users[i]
+      case "ice_candidate":
+        let userToReceiveIceCandidate = findUser(data.target);
+        if (userToReceiveIceCandidate) {
+          userToReceiveIceCandidate.conn.send(
+            JSON.stringify({
+              type: "ice_candidate",
+              name: data.name,
+              data: {
+                sdpMLineIndex: data.data.sdpMLineIndex,
+                sdpMid: data.data.sdpMid,
+                sdpCandidate: data.data.sdpCandidate,
+              },
+            })
+          );
+        }
+        break;
     }
-}
+  });
+
+  connection.on("close", () => {
+    userMap.forEach((key, value) => {
+      if (value.conn === connection) {
+        userMap.delete(key);
+      }
+    });
+  });
+});
+
+const findUser = (username) => {
+  return userMap.get(username);
+};
